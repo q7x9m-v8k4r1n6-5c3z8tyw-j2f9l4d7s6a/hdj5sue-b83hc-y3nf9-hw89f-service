405fac4c5a39a4ca0c87ab42a2144ff72742bf72
hoangdada <luongthehoang010206@gmail.com>
2026-07-22 00:36:58 +0700

    refactor: chuyển LoginResultModal từ LoginCommand.cs sang folder DTOs, part 2

--- src/OVCMOVE.Application/Features/Auth/Command/Login/LoginCommand.cs
+++ src/OVCMOVE.Application/Features/Auth/Command/Login/LoginCommand.cs
@@ -3,12 +3,6 @@ using OVCMOVE.Application.DTOs.ResultModels;
 
 namespace OVCMOVE.Application.Features.Auth.Command.Login;
 
-public record LoginResult( 
-    string AccessToken,
-    string RefreshToken,
-    DateTime AccessTokenExpiration,
-    Guid UserId);
-
 public record LoginCommand(
     string Username,
     string Password) : IRequest<LoginResultModel>; 

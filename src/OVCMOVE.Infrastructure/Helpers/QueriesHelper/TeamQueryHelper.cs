fafcfefb2cca471b67fffd13999256d6726fce59
mpuc090806 <phuc0908061@gmail.com>
2026-07-24 00:18:44 +0700

    adjustment

--- src/OVCMOVE.Infrastructure/Helpers/QueriesHelper/TeamQueryHelper.cs
+++ src/OVCMOVE.Infrastructure/Helpers/QueriesHelper/TeamQueryHelper.cs
@@ -5,20 +5,37 @@ public static class TeamQueries
     public static string GetAllTeamsQuery()
     {
         return @"
-            SELECT Id, Name, LeaderEmail, Username, Status
-            FROM [dbo].[Team] WITH (NOLOCK)
-            ORDER BY Name;
+            SELECT
+                t.Id,
+                COALESCE(NULLIF(u.DisplayName, ''), u.Username) AS Name,
+                u.Email AS LeaderEmail,
+                u.Username,
+                u.Status
+            FROM [dbo].[Teams] t WITH (NOLOCK)
+            INNER JOIN [dbo].[Users] u WITH (NOLOCK) ON u.Id = t.UserId
+            WHERE u.IsDeleted = 0
+            ORDER BY COALESCE(NULLIF(u.DisplayName, ''), u.Username);
         ";
     }
 
     public static string SearchTeamQuery()
     {
         return @"
-            SELECT Id, Name, LeaderEmail, Username, Status
-            FROM [dbo].[Team] WITH (NOLOCK)
-            WHERE Name LIKE @Keyword
-               OR Username LIKE @Keyword
-            ORDER BY Name;
+            SELECT
+                t.Id,
+                COALESCE(NULLIF(u.DisplayName, ''), u.Username) AS Name,
+                u.Email AS LeaderEmail,
+                u.Username,
+                u.Status
+            FROM [dbo].[Teams] t WITH (NOLOCK)
+            INNER JOIN [dbo].[Users] u WITH (NOLOCK) ON u.Id = t.UserId
+            WHERE u.IsDeleted = 0
+              AND (
+                COALESCE(NULLIF(u.DisplayName, ''), u.Username) LIKE @Keyword
+                OR u.Username LIKE @Keyword
+                OR u.Email LIKE @Keyword
+              )
+            ORDER BY COALESCE(NULLIF(u.DisplayName, ''), u.Username);
         ";
     }
 }
